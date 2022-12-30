01f533a5cba56237de623dcba073ba2b92106825
Renáta Irinyi <103267773+ReniIrinyi@users.noreply.github.com>
2022-12-30 09:06:04 +0100

    c

--- 01-expense-tracker/src/App.js
+++ 01-expense-tracker/src/App.js
@@ -1,10 +1,37 @@
-import ExpenseItem from "./components/Expenseltem";
+import Expenses from "./components/Expenses";
 
 function App() {
+
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(),
+    },
+    { 
+      id: 'e2', 
+      title: 'New TV', 
+      amount: 799.49, 
+      date: new Date() },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(),
+    },
+  ];
+
   return(
     <div>
 <h2>Let´s get started</h2>
-<ExpenseItem></ExpenseItem>
+<Expenses items={expenses}></Expenses>
 </div>
   )
 }
